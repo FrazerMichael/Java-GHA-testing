0e3bce18a969e256e970efbfc5fcb17ec89179f1
Michael Frazer <mjfrazer1@gmail.com>
2024-07-17 22:49:54 -0400

    small code change

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -7,7 +7,10 @@ package com.example;
 public class App 
 {
     public static void main( String[] args )
+    //Making an update to the code
     {
         System.out.println( "Hello World!" );
     }
 }
+
+
